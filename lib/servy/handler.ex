a36e0c433bafa35b6d113aa9562d1c5e6e90699f
Utsa Varia <utsav99varia@gmail.com>
2021-04-01 16:55:34 +0530

    ENH: Function Clauses
    Add funcitons to handle multiple routes

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,9 +2,12 @@ defmodule Servy.Handler do
     def handle(request) do
         request
         |> parse
+        |> log
         |> route
         |> format_response
     end
+
+    def log(conv), do: IO.inspect conv
 
     def parse(request) do
         [method, path, _] = 
@@ -16,7 +19,15 @@ defmodule Servy.Handler do
     end
     
     def route(conv) do
+        route(conv, conv.method, conv.path)
+    end
+
+    def route(conv, "GET", "/wildthings") do
         %{conv | resp_body: "Bears, Lions, Tigers"}
+    end
+
+    def route(conv, "GET", "/bears") do
+        %{conv | resp_body: "Teddy, Smoking, Padding"}
     end
     
     def format_response(conv) do
@@ -33,6 +44,27 @@ end
 
 request = """
 GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+IO.puts Servy.Handler.handle(request)
+
+request = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+IO.puts Servy.Handler.handle(request)
+
+
+request = """
+GET /bigfoot HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
