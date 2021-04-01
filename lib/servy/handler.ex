45f4086acae77e05c1505da86dc9770931e72c60
Utsa Varia <utsav99varia@gmail.com>
2021-04-01 19:31:11 +0530

    ENH: Map pattern matching
    Introduce map pattern matching on different urls

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,10 +2,18 @@ defmodule Servy.Handler do
     def handle(request) do
         request
         |> parse
+        |> rewrite_path
         |> log
         |> route
+        |> track
         |> format_response
     end
+
+    def rewrite_path(%{path: "/wildlife"} = conv) do
+        %{ conv | path: "/wildthings" }
+    end
+
+    def rewrite_path(conv), do: conv
 
     def log(conv), do: IO.inspect conv
 
@@ -22,30 +30,33 @@ defmodule Servy.Handler do
             status: nil 
         }
     end
-    
-    def route(conv) do
-        route(conv, conv.method, conv.path)
-    end
 
-    def route(conv, "GET", "/wildthings") do
+    def route(%{method: "GET", path: "/wildthings"} = conv) do
         %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
     end
 
-    def route(conv, "GET", "/bears") do
+    def route(%{method: "GET", path: "/bears"} = conv) do
         %{ conv | status: 200, resp_body: "Teddy, Smoking, Padding" }
     end
 
-    def route(conv, "GET", "/bears" <> id) do
+    def route(%{method: "GET", path: "/bears" <> id} = conv) do
         %{ conv | status: 200, resp_body: "Bear #{id}"}
     end
 
-    def route(conv, "DELETE", "/bears/" <> _id) do
+    def route(%{method: "DELETE", path: "/bears/" <> _id} = conv) do
         %{ conv | status: 403, resp_body: "Deleting a bear is forbidden!"}
-      end
+    end
 
-    def route(conv, _method, path) do
+    def route(%{path: path} = conv) do
         %{ conv | status: 404, resp_body: "No #{path} here!" }
     end
+
+    def track(%{status: 404, path: path} = conv) do
+        IO.puts "Warning: #{path} is on the loose!"
+        conv
+    end
+
+    def track(conv), do: conv
     
     def format_response(conv) do
         # TODO: Use values in the map to create an HTTP response string:
@@ -105,6 +116,16 @@ IO.puts Servy.Handler.handle(request)
 
 request = """
 DELETE /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+IO.puts Servy.Handler.handle(request)
+
+request = """
+GET /wildlife HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
