158b4b35f9b876dc3343ce8be55b7340a0b4bb5b
Utsa Varia <utsav99varia@gmail.com>
2021-04-01 12:12:02 +0530

    ENH: Pattern Matching
    Make parse function to get request details in handler

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,9 +1,5 @@
 defmodule Servy.Handler do
     def handle(request) do
-        # conv = parse(request)
-        # conv = route(request)
-        # format_response(request)
-
         request
         |> parse
         |> route
@@ -11,8 +7,12 @@ defmodule Servy.Handler do
     end
 
     def parse(request) do
-        # TODO: Parse the request string into a map:
-        conv = %{ method: "GET", path: "/wildthings", resp_body: "" }
+        [method, path, _] = 
+            request 
+            |> String.split("\n") 
+            |> List.first
+            |> String.split
+        %{ method: method, path: path, resp_body: "" }
     end
     
     def route(conv) do
